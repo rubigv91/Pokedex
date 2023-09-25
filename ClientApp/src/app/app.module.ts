ae0dc5daef568a782e7c6ed031193e0a2a3ec741
Rubi <rubi.gv91@gmail.com>
2023-09-25 12:04:47 -0600

    first commit

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { LoginComponent } from './components/login/login.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ClientApp/src/app/models/users.ts
@@ -0,0 +1,8 @@
+export interface Users{ 
+    nombre:string;
+    apellido:string;
+    email:string;
+    password:string;
+    fechaNacimiento:Date;
+    equipo:string;
+}
